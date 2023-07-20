74fb2e7b74766a72fbca5709b06792932da3652f
Dong Liang <dong.liang.1@stonybrook.edu>
2023-07-19 22:47:18 -0400

    changed to hashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, Link } from "react-router-dom";
 import SearchParams from "./SearchParam";
 import Details from "./Details";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
@@ -18,7 +18,7 @@ const queryClient = new QueryClient({
 const App = () => {
   const adoptedPet = useState(null);
   return (
-    <BrowserRouter>
+    <Router>
       <QueryClientProvider client={queryClient}>
         <AdoptedPetContext.Provider value={adoptedPet}>
         <header>
@@ -30,7 +30,7 @@ const App = () => {
         </Routes>
         </AdoptedPetContext.Provider>
       </QueryClientProvider>
-    </BrowserRouter>
+    </Router>
   );
 };
 
